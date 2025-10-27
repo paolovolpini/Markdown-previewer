ffbcf12fab4530c4501101ffd57dc1b1ef3f0c33
bored_dam <damianotrovato122@gmail.com>
2025-10-27 23:50:22 +0100

    added src folder

---
+++ src/file-handler.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+
+    std::fstream input_file;
+    
+    /* i think we could print the actual syntax here. or maybe we could have support for a -help flag*/
+    if (argc < 2) {
+        std::cerr << "not enough arguments." << std::endl;
+        return(EXIT_FAILURE);
+    }
+
+    /* checking for file extension */
+    std::string file_path(argv[1]);
+    if (file_path.length() < 3) {
+        std::cerr << "input file is not a .md file." << std::endl;
+        return(EXIT_FAILURE);
+    }
+
+    if (!(file_path.substr(file_path.length()-3, 3) == ".md")) {
+        std::cerr << "input file is not a .md file." << std::endl;
+        return(EXIT_FAILURE);
+    }
+
+    input_file.clear();
+
+    input_file.open(argv[1]);
+    if (!input_file.is_open()) {
+        std::cerr << "error: file was not properly opened, probably does not exists." << std::endl;
+        return(EXIT_FAILURE);
+    } 
+
+    /* stuff */
+    
+    /* should we use a data structure to pass the lines to the tokenizer? */
+    input_file.close();
+
+
+}
